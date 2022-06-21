6f652a32c8ca58e6a9c8c49a8748f13845ebd901
ARoble <abdulladif0042@gmail.com>
2022-06-21 15:12:10 +0300

    final

--- pages/Partials/PageHead.js
+++ pages/Partials/PageHead.js
@@ -5,6 +5,14 @@ function PageHead({ title }) {
     <Head>
       <title>{title}</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta
+        name="keywords"
+        content="bootcamp, fikrcamp, somaliland, coding, somali, javascript, training, somaliland coding, somaliland tech, tech, hargeisa, full stack"
+      ></meta>
+      <meta
+        name="description"
+        content="Fikrcamp is the first coding bootcamp in Somaliland. Attending this bootcamp students are given access to mentors, awesome work environment and most importantly experienced tutors."
+      ></meta>
       <link rel="shortcut icon" href="./faviconn.PNG" />
     </Head>
   );
